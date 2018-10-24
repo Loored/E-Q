e2792713f6a60f1c1bcdf4705b6f6c33c0b28205
AlejandroGTA <36650624+AlejandroGTA@users.noreply.github.com>
2018-10-24 17:12:28 -0500

    Big update
    Tearing ourselves apart

---
+++ src/routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+const passport = require('passport');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+  // res.render('index', { title: 'Express' });
+});
+
+router.get('/signup',function(req,res,next){
+  res.render('signup');
+  // res.end("It's up");
+});
+
+router.post('/signup', passport.authenticate('local-signup',{
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true
+}));
+
+router.get('/signin',function(req,res,next){
+  res.render('signin');
+});
+
+router.post('/signin', passport.authenticate('local-signin', {
+  successRedirect: '/profile',
+  failureRedirect: '/signin',
+  failureFlash: true
+}));
+
+router.get('/profile', isAuthenticated, function (req, res, next) {
+  res.render('profile');
+});
+
+// Depsues de aqui estan restricciones / protegidas
+
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
+
+function isAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/')
+}
+
+module.exports = router;
